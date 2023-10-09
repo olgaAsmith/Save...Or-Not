ba976e85f2892154794b5f8f9440d16ebe799c3b
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-10-09 22:19:52 +0300

    add subscibe

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -11,18 +11,18 @@ export default function Features() {
         <ul className={styles.features__list}>
           <li className={styles.features__item}>
               <h4 className={`${bebas.className} ${styles.features__name}`}>SURVIVE AT ALL COSTS</h4>
-              <p className={styles.features__text}>You have 30 minutes to find a relic, signal for extraction, and grab one of three spots on the rescue chopper.</p>
+              <p className={`${styles.features__text} text`}>You have 30 minutes to find a relic, signal for extraction, and grab one of three spots on the rescue chopper.</p>
               <span className={styles.features__circle}></span>
               <span className={styles.features__line}></span>          
           </li>
           <li className={styles.features__item}>
               <h4 className={`${bebas.className} ${styles.features__name}`}>CREATE ALLIES & ENEMIES</h4>
-              <p className={styles.features__text}>Forge alliances with fellow survivors to increase your chances of escape, but be wary—betrayals can happen when desperation sets in.</p>
+              <p className={`${styles.features__text} text`}>Forge alliances with fellow survivors to increase your chances of escape, but be wary—betrayals can happen when desperation sets in.</p>
               <span className={styles.features__circle}></span>
           </li>
           <li className={styles.features__item}>
             <h4 className={`${bebas.className} ${styles.features__name}`}>IMPRESS THE AUDIENCE</h4>
-            <p className={styles.features__text}>Show off your survival skills and make daring moves to win over the audience. The more they like you, the more likely they are to support your bid for freedom.</p>
+            <p className={`${styles.features__text} text`}>Show off your survival skills and make daring moves to win over the audience. The more they like you, the more likely they are to support your bid for freedom.</p>
             <span className={styles.features__circle}></span>
           </li>
         </ul>
--- src/app/requirements/page.tsx
+++ src/app/requirements/page.tsx
@@ -9,23 +9,23 @@ export default function Requirements() {
       <ul className={styles.requirements__list}>
         <li className={styles.requirements__element}>
           <p className={`${bebas.className} ${styles.requirements__item}`}>OS:</p>
-          <p className={styles.requirements__text}>Windows 7 64-bit only (No OSX support at this time)</p>
+          <p className={`${styles.requirements__text} text`}>Windows 7 64-bit only (No OSX support at this time)</p>
         </li>
         <li className={styles.requirements__element}>
           <p className={`${bebas.className} ${styles.requirements__item}`}>pROCESSOR:</p>
-          <p className={styles.requirements__text}>Intel Core 2 Duo @ 2.4 GHZ or AMD Athlon X2 @ 2.8 GHZ</p>
+          <p className={`${styles.requirements__text} text`}>Intel Core 2 Duo @ 2.4 GHZ or AMD Athlon X2 @ 2.8 GHZ</p>
         </li>
         <li className={styles.requirements__element}>
           <p className={`${bebas.className} ${styles.requirements__item}`}>mEMORY:</p>
-          <p className={styles.requirements__text}>8GB RAM</p>
+          <p className={`${styles.requirements__text} text`}>8GB RAM</p>
         </li>
         <li className={styles.requirements__element}>
           <p className={`${bebas.className} ${styles.requirements__item}`}>storage:</p>
-          <p className={styles.requirements__text}>8GB available space</p>
+          <p className={`${styles.requirements__text} text`}>8GB available space</p>
         </li>
         <li className={`${styles.requirements__element} ${styles.requirements__longer}`}>
           <p className={`${bebas.className} ${styles.requirements__item}`}>GRAPHICS:</p>
-          <p className={styles.requirements__text}>NVIDIA GeForce GTX 660 2GB or AMD Radeon HD 7850 2GB DirectX11 (Shader Model 5)</p>
+          <p className={`${styles.requirements__text} text`}>NVIDIA GeForce GTX 660 2GB or AMD Radeon HD 7850 2GB DirectX11 (Shader Model 5)</p>
         </li>
       </ul>
     </section>
